c1aef9b8b10a0b70a167667edf74658f29d148b3
shagirul <shagirul@gmail.com>
2025-01-29 21:29:28 +0530

    warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" }, // Suppresses common TS warnings
+  },
+});
